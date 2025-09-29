f0d4e0491a5ffd12c8c6b6086e155e45fcb21048
Dharmil Trivedi <dharmiltrivedi5@gmail.com>
2025-09-29 17:48:22 +0530

    ADD: Header

---
+++ lib/constants.ts
@@ -0,0 +1,10 @@
+export const NAV_ITEMS = [
+    { href: '/', title: 'Dashboard' },
+    { href: '/search', title: 'Search' },
+    { href: '/watchlist', title: 'Watchlist' },
+    // { href: '/alerts', title: 'Alerts' },
+    // { href: '/settings', title: 'Settings' },
+    // { href: '/profile', title: 'Profile' },
+    // { href: '/logout', title: 'Logout' },
+]
+
